a71d0d959c8b09afe77db9b80889be1b7801fa70
dcfabian <fabian@datachecker.nl>
2023-03-30 14:56:05 +0200

    speed improvement

--- html/js/language/en.js
+++ html/js/language/en.js
@@ -1,16 +1,16 @@
 const LANGUAGE = {
     "start_prompt": "Tap to start.",
-    "no_face": "No face detected,\n please move your head into the frame.",
+    "no_face": "No face detected,\nplease position your face in the frame.",
     "nod_head": "Please nod your head slightly.",
-    "face_thresh": "Face not fully visible.\nMake sure your face is not covered.",
-    "face_far": "Please move closer.",
-    "face_close": "Face too close, \nplease move slightly away.",
-    "exp_dark": "The image is too dark.\nFind an environment with more light.",
-    "exp_light": "The image is too light.\nFind an environment with less light.",
-    "blur": "Blur detected, \nplease stay still.",
+    "face_thresh": "Face not clearly visible.\nEnsure better lighting conditions\nor make sure your face is not covered.",
+    "face_far": "Please move closer to the camera.",
+    "face_close": "Face too close,\nplease move slightly away.",
+    "exp_dark": "The image is too dark.\nFind a well-lit environment.",
+    "exp_light": "The image is too light.\nFind a dimmer environment.",
+    "blur": "Image is not sharp,\nplease stay still.",
     "capture_error": "We could not capture an image.\nAccess to the camera is required.",
     "challenge_1": "Slowly move your face up.",
     "challenge_2": "Slowly move your face to the right.",
     "challenge_3": "Slowly move your face down.",
     "challenge_4": "Slowly move your face to the left."
-}
+    }
